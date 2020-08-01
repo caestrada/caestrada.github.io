fb4102c7ab923287416b4b910b310fbaa319dab0
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-01 07:55:08 -0400

    Setup problem 01

---
+++ programming-problems/problem01.js
@@ -0,0 +1,11 @@
+/*
+  Write a function that takes two strings, s1 and s2 and return the 
+  longest common subsequence of s1 and s2.
+
+  ex:
+  "ABAZDC", "BACBAD"  => "ABAD"
+  "AGGTAB", "GXTXAYB" => "GTAB"
+  "aaaa", "aa"        => "aa"
+*/
+
+console.log('Hello, world');
