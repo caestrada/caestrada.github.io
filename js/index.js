1144169374faf0e725b93c58bdfa981eb94e0071
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-03 08:11:06 -0400

    Update header

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 
 setInterval(() => {
-    const avatarImg = document.querySelector('.avatar-image');
-    let classes = avatarImg.className.split(' ');
-    classes = (classes[1] === 'avatar-transition-dark')
-                ? [classes[0], 'avatar-transition-light'] 
-                : [classes[0], 'avatar-transition-dark'];
-    avatarImg.className = `${classes[0]} ${classes[1]}`;
+    const avatarImgs = document.querySelectorAll('.avatar-image');
+    avatarImgs.forEach((avatarImg) => {
+        let classes = avatarImg.className.split(' ');
+        classes = (classes[1] === 'avatar-transition-dark')
+                    ? [classes[0], 'avatar-transition-light'] 
+                    : [classes[0], 'avatar-transition-dark'];
+        avatarImg.className = `${classes[0]} ${classes[1]}`;
+    });
 }, 500);
