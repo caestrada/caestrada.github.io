91840f1cffd23efc1dc5b533bca205a7691ca797
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-19 11:01:45 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1dba04d40e8140166e12";
+/******/ 	var hotCurrentHash = "0eef4306c6ff8e049bf3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
